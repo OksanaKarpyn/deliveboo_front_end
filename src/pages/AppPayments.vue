16118c36b939e5d19073e4d2357e883ceb4da9c0
Massimiliano Fanizza <fanizzamassimiliano@gmail.com>
2023-12-13 12:03:59 +0100

    working view of purchase with braintree

--- src/pages/AppPayments.vue
+++ src/pages/AppPayments.vue
@@ -1,92 +1,101 @@
-<script>
-import  { store } from "../data/store";
-export default {
-  name: 'AppPayments',
-  data() {
-    return {
+  <script>
+  import { store } from "../data/store";
+  export default {
+    name: 'AppPayments',
+    data() {
+      return {
         store
-    }
-  },
-  methods: {
-    payment() {
-        console.log("ciao");
-        const token = '8xctsvwwvdbyksqp';
-        braintree.dropin.create({
-        authorization: token,
-        container: '#dropin-container'
-    }, (error, dropinInstance) => {
-        if (error) console.error(error);
+      }
+    },
+    methods: {
+        payment() {
+            console.log("ciao");
+            const token = '8xctsvwwvdbyksqp';
 
-        form.addEventListener('submit', event => {
-            event.preventDefault();
-
-            dropinInstance.requestPaymentMethod((error, payload) => {
+            braintree.dropin.create({
+                authorization: token,
+                container: '#dropin-container'
+            }, (error, dropinInstance) => {
                 if (error) console.error(error);
 
-                document.getElementById('nonce').value = payload.nonce;
-                form.submit();
-            });
-        });
-    });
-    },
-    paymentJs() {
-        var button = document.querySelector('#submit-button');
+                // Assuming you have a form with the ID 'payment-form'
+                const form = document.getElementById('payment-form');
 
-        braintree.dropin.create({
-        authorization: 'sandbox_g42y39zw_8xctsvwwvdbyksqp',
-        selector: '#dropin-container'
-        }, function (err, instance) {
-        button.addEventListener('click', function () {
-            instance.requestPaymentMethod(function (err, payload) {
-            // Submit payload.nonce to your server
+                form.addEventListener('submit', event => {
+                event.preventDefault();
+
+                dropinInstance.requestPaymentMethod((error, payload) => {
+                    if (error) console.error(error);
+
+                    document.getElementById('nonce').value = payload.nonce;
+                    form.submit();
+                });
+                });
             });
-        })
-    });
+        },
+
+        paymentJs() {
+          var button = document.querySelector('#submit-button');
+    
+          braintree.dropin.create({
+              authorization: 'sandbox_g42y39zw_348pk9cgf3bgyw2b',
+            selector: '#dropin-container'
+          }, (err, instance) => {
+            instance.requestPaymentMethod((err, payload) => {
+              // Submit payload.nonce to your server
+              console.log(payload);
+            });
+          });
+        
+      },
+      },
+    mounted() {
+      this.payment();
+      this.paymentJs();
     }
-  },
-  mounted() {
-    this.payment();
   }
-}
-</script>
-
+  </script>
 <template>
-    <div id="dropin-container"></div>
-    <button id="submit-button" @click="this.payment()" class="button button--small button--green">Purchase</button>
-</template>
-
-<style scoped lang="scss">
+    <div>
+      <div id="dropin-container"></div>
+      <button @click="paymentJs(), payment()" class="button button--small button--green">Purchase</button>
+    </div>
+  </template>
+  
+  
+  <style scoped lang="scss">
     .button {
-    cursor: pointer;
-    font-weight: 500;
-    left: 3px;
-    line-height: inherit;
-    position: relative;
-    text-decoration: none;
-    text-align: center;
-    border-style: solid;
-    border-width: 1px;
-    border-radius: 3px;
-    -webkit-appearance: none;
-    -moz-appearance: none;
-    display: inline-block;
+      cursor: pointer;
+      font-weight: 500;
+      left: 3px;
+      line-height: inherit;
+      position: relative;
+      text-decoration: none;
+      text-align: center;
+      border-style: solid;
+      border-width: 1px;
+      border-radius: 3px;
+      -webkit-appearance: none;
+      -moz-appearance: none;
+      display: inline-block;
     }
-
+  
     .button--small {
-    padding: 10px 20px;
-    font-size: 0.875rem;
+      padding: 10px 20px;
+      font-size: 0.875rem;
     }
-
+  
     .button--green {
-    outline: none;
-    background-color: #64d18a;
-    border-color: #64d18a;
-    color: white;
-    transition: all 200ms ease;
+      outline: none;
+      background-color: #64d18a;
+      border-color: #64d18a;
+      color: white;
+      transition: all 200ms ease;
     }
-
+  
     .button--green:hover {
-    background-color: #8bdda8;
-    color: white;
+      background-color: #8bdda8;
+      color: white;
     }
-</style>
+  </style>
+  
