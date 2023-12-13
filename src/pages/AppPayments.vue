3aa691bc668fcccd0e8898fe31bd002a8a961768
Oksana Karpyn <oksana101992@gmail.com>
2023-12-13 12:29:56 +0100

    paymant

--- src/pages/AppPayments.vue
+++ src/pages/AppPayments.vue
@@ -4,62 +4,123 @@
     name: 'AppPayments',
     data() {
       return {
-        store
+        store,
+        token: 'sandbox_g42y39zw_348pk9cgf3bgyw2b', 
       }
     },
     methods: {
-        payment() {
-            console.log("ciao");
-            const token = '8xctsvwwvdbyksqp';
 
-            braintree.dropin.create({
-                authorization: token,
-                container: '#dropin-container'
-            }, (error, dropinInstance) => {
-                if (error) console.error(error);
 
-                // Assuming you have a form with the ID 'payment-form'
-                const form = document.getElementById('payment-form');
-
-                form.addEventListener('submit', event => {
-                event.preventDefault();
-
-                dropinInstance.requestPaymentMethod((error, payload) => {
-                    if (error) console.error(error);
-
-                    document.getElementById('nonce').value = payload.nonce;
-                    form.submit();
-                });
-                });
-            });
+        initializeBraintree() {
+      braintree.dropin.create(
+        {
+          authorization: this.token,
+          container: '#dropin-container',
         },
+        (error, dropinInstance) => {
+          if (error) console.error(error);
 
-        paymentJs() {
-          var button = document.querySelector('#submit-button');
+          this.$data.dropinInstance = dropinInstance;
+        }
+      );
+    },
+    submitPayment() {
+      this.dropinInstance.requestPaymentMethod((error, payload) => {
+        if (error) {
+          console.error(error);
+          // Handle the error as needed
+          return;
+        }
+
+        // Now you can perform additional actions with the payload if needed
+        console.log(payload);
+
+        // Send the payment details to your server
+        this.sendPaymentDetails(payload);
+      });
+    },
+    sendPaymentDetails(payload) {
+      // Make an HTTP request to your server to handle the payment
+      // You can use libraries like Axios or the native Fetch API
+      // Example using Axios:
+      // axios.post('/your-server-endpoint', { nonce: payload.nonce })
+      //   .then(response => {
+      //     // Handle the server response
+      //   })
+      //   .catch(error => {
+      //     console.error(error);
+      //   });
+
+      // For demonstration purposes, log a message here
+      console.log('Sending payment details to the server:', payload);
+
+      // Add any other logic you need after sending payment details
+    },
+  
+
+    //     payment() {
+    //         console.log("ciao");
+    //         const token = '8xctsvwwvdbyksqp';
+
+    //         braintree.dropin.create({
+    //             authorization: token,
+    //             container: '#dropin-container'
+    //         }, (error, dropinInstance) => {
+    //             if (error) console.error(error);
+
+    //             // Assuming you have a form with the ID 'payment-form'
+    //             const form = document.getElementById('payment-form');
+
+    //             form.addEventListener('submit', event => {
+    //             event.preventDefault();
+
+    //             dropinInstance.requestPaymentMethod((error, payload) => {
+    //                 if (error) console.error(error);
+
+    //                 document.getElementById('nonce').value = payload.nonce;
+    //                 form.submit();
+    //             });
+    //             });
+    //         });
+    //     },
+
+    //     paymentJs() {
+    //       var button = document.querySelector('#submit-button');
     
-          braintree.dropin.create({
-              authorization: 'sandbox_g42y39zw_348pk9cgf3bgyw2b',
-            selector: '#dropin-container'
-          }, (err, instance) => {
-            instance.requestPaymentMethod((err, payload) => {
-              // Submit payload.nonce to your server
-              console.log(payload);
-            });
-          });
+    //       braintree.dropin.create({
+    //           authorization: 'sandbox_g42y39zw_348pk9cgf3bgyw2b',
+    //         selector: '#dropin-container'
+    //       }, (err, instance) => {
+    //         instance.requestPaymentMethod((err, payload) => {
+    //           // Submit payload.nonce to your server
+    //           console.log(payload);
+    //         });
+    //       });
         
-      },
+    //   },
       },
     mounted() {
-      this.payment();
-      this.paymentJs();
+        this.initializeBraintree();
+      //this.payment();
+      //this.paymentJs();
     }
   }
   </script>
 <template>
     <div>
+        <div id="dropin-container"></div>
+        <form @submit.prevent="submitPayment">
+          <!-- Other form fields can go here -->
+          <button type="submit">Submit Payment</button>
+        </form>
+      </div>
+
+
+
+    <!-- <div>
       <div id="dropin-container"></div>
       <button @click="paymentJs(), payment()" class="button button--small button--green">Purchase</button>
-    </div>
+    </div> -->
   </template>
   
   
